c523e94ce198939884ab8f8ee4789b6ba743ddeb
Marcus <marcus.scott.belcher@gmail.com>
2019-03-31 22:50:07 +0100

    scaffold and webgl example

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Main from "./components/Main";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Main />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/CSS.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "./Layout";
+
+const Component = () => (
+  <Layout>
+    <p>CSS</p>
+  </Layout>
+);
+
+export default Component;
